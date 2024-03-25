1e3263021d4ca85c7d901148306c0453df969616
Darlinkion <htdhytfhyfmtftyftmy@yandex.ru>
2024-03-25 21:03:11 +0300

    7 спринт, исправлены ошибки в истории и тестах. Исправил ошибку чекстайла v2.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
-import model.Epic;
-import model.Status;
-import model.SubTask;
-import model.Task;
-import service.InMemoryTaskManager;
-import service.TaskManager;
-
 public class Main {
-
     public static void main(String[] args) {
     }
 }
